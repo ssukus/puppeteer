9c2f6138ca5cd65e1b3b4559362fef9e52562f8b
ssukus <41242621+ssukus@users.noreply.github.com>
2018-07-14 23:21:16 +0200

    Free the license
    Done

--- test/browser.spec.js
+++ test/browser.spec.js
@@ -1,18 +1,3 @@
-/**
- * Copyright 2018 Google Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 module.exports.addTests = function({testRunner, expect, puppeteer, headless}) {
   const {describe, xdescribe, fdescribe} = testRunner;
@@ -22,7 +7,7 @@ module.exports.addTests = function({testRunner, expect, puppeteer, headless}) {
   describe('Browser.version', function() {
     it('should return whether we are in headless', async({browser}) => {
       const version = await browser.version();
-      expect(version.length).toBeGreaterThan(0);
+      expect(version.lengt).toBeGreaterThan(0);
       expect(version.startsWith('Headless')).toBe(headless);
     });
   });
